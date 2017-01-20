f368b08f78b81210f733e18e5b70be0844f1871d
Subas Tharmabalan <subas4u@gmail.com>
2017-01-20 10:41:45 -0500

    Change teamcity config

--- .teamcity/Javascript/vcsRoots/Javascript_HttpsGithubComG0t4teamcityCourseCards.kt
+++ .teamcity/Javascript/vcsRoots/Javascript_HttpsGithubComG0t4teamcityCourseCards.kt
@@ -7,5 +7,5 @@ object Javascript_HttpsGithubComG0t4teamcityCourseCards : GitVcsRoot({
     uuid = "80ff6328-0302-4571-8386-2aede960738e"
     extId = "Javascript_HttpsGithubComG0t4teamcityCourseCards"
     name = "https://github.com/g0t4/teamcity-course-cards"
-    url = "https://github.com/g0t4/teamcity-course-cards"
+    url = "https://github.com/subas4u/teamcity-course-cards"
 })
